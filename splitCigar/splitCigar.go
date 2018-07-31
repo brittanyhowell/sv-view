93c8d5bc2a79dfc19592ac3bdd3f495d0e668400
brittanyhowell <brittany.howell1@gmail.com>
2018-07-31 14:58:06 +0100

    Revised from readCigar.go Reads table taken from R. Currently just prints with no manipulation. Basically, this is the framework.

---
+++ splitCigar/splitCigar.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var (
+		binFile string
+		outPath string
+		outFile string
+	)
+	type binnedReadsTable struct {
+		name   string
+		start  int
+		end    int
+		width  int
+		bin    string
+		cigar  string
+		mScore string
+		aScore string
+		flags  string
+	}
+
+	// For reference,
+	//the first is the variable,
+	//the second is the flag for the call command,
+	//the third is default,
+	//the fourth is the description
+	flag.StringVar(&binFile, "binFile", "", "name of the file with read data & bin values")
+	flag.StringVar(&outPath, "outPath", "", "path to output DIR")
+	flag.StringVar(&outFile, "outFile", "", "Name of output file")
+	flag.Parse()
+
+	fmt.Println("Begin the expansion!")
+
+	file := fmt.Sprintf("%v/%v", outPath, outFile)
+	out, err := os.Create(file)
+	if err != nil {
+		log.Fatalf("failed to create out %s: %v", file, err)
+	}
+	defer out.Close()
+
+	// Read in the binned reads table
+	fIn, err := os.Open(binFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fIn.Close()
+
+	var readIn []string
+	sIn := bufio.NewScanner(fIn)
+
+	iInt := 0
+	for sIn.Scan() {
+		if iInt > 0 { // IMPORTANT - ASSUMES THERE IS A HEADER, ELSE IT WILL SKIP THE FIRST LINE
+			readIn = append(readIn, sIn.Text())
+		}
+		iInt++
+	}
+	if err := sIn.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, rIn := range readIn {
+		rsplit := strings.Split(rIn, "\t")
+
+		rName := rsplit[0]
+		rBin := rsplit[4]
+		rCigar := rsplit[5]
+		rMapq := rsplit[6]
+		rAS := rsplit[7]
+		rFlags := rsplit[8]
+
+		rStartS, _ := strconv.ParseFloat(rsplit[1], 1)
+		rEndS, _ := strconv.ParseFloat(rsplit[2], 1)
+		rWidthS, _ := strconv.ParseFloat(rsplit[3], 1)
+
+		rStart := int(rStartS)
+		rEnd := int(rEndS)
+		rWidth := int(rWidthS)
+
+		r := binnedReadsTable{
+			name:   rName,
+			start:  rStart,
+			end:    rEnd,
+			width:  rWidth,
+			bin:    rBin,
+			cigar:  rCigar,
+			mScore: rMapq,
+			aScore: rAS,
+			flags:  rFlags,
+		}
+
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+			r.name,
+			r.start,
+			r.end,
+			r.width,
+			r.bin,
+			r.cigar,
+			r.mScore,
+			r.aScore,
+			r.flags)
+
+	}
+
+}
