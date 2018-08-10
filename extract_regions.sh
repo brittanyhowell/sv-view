e143c936fa4838c1400d648bb1275034b53ff7c7
brittanyhowell <brittany.howell1@gmail.com>
2018-08-10 17:05:26 +0100

    A good job so far Wow I think this is in a good place to run the scripts. I just need to do a couple more things. Noted in the comment line. Also, I have yet to test this (it might be full of bugs)

--- extract_regions.sh
+++ extract_regions.sh
@@ -1,95 +1,151 @@
+#!/bin/bash
+
+## what I need to do: 
+# Make files per sample
+# add a reporting thing, to count how many SVs of each type in the CNV thing. please. 
+
+
 ##Extract specified regions from bamfile
 
-# Input files required: 
-# Bam files per sample
-# Interval table per software. 
+## This script takes output from Genome STRiP - CNV pipeline (more to be added later) and prints a plot of reads in the SVs per sample. 
+## Why? Because it's nice to visualise what the SV caller saw, and therefore what it calls as an SV, to train yourself on what a "real" one might be.
 
+## Input files needed: 
+# Interval table from CNV, with columns as arranged in github.com/brittanyhowell/sv-detect/blob/master/genomestrip/extract_genotyped_cnv.sh
+# Bam file used, with index
 
-# Input callset software type:
-software="CNV" 
-# DIRs:
-# Scripts:
-scriptDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/scripts/
-# Folder for intervals: 
-intDIR="/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/intervals_${software}"
+## IniTiALisE
 
+    # Scripts and junk
+    scriptDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/scripts/
+    wkDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/viewSV-out/
 
+    # Raw inputs
+    bamDIR=/lustre/scratch115/projects/interval_wgs/testBams/
+    STinDIR=/Users/bh10/Documents/Rotations/Rotation3/data/testView/
+    sample="EGAN00001214492" ## Submit a job array per sample, please Brie
 
-# DIR for interval lists per sample
-if [ -d $intDIR ]; then
-    echo "Folder $intDIR exists ...replacing" 
-    rm -rf $intDIR
-    mkdir $intDIR
-else
-    mkdir $intDIR
-    echo "Folder $intDIR does not exist ... creating"     
-    
-fi
+    # settings
+    software="CNV" 
+    whichSVs="refOnly" # options include "noRef" or "all" (actually right now it's noRef or nothing..)
 
-# DIR for reads per interval
-if [ -d $svReadsDIR ]; then
-    echo "Folder $svReadsDIR exists ...replacing" 
-    rm -rf $svReadsDIR
-    mkdir $svReadsDIR
-else
-    mkdir $svReadsDIR
-    echo "Folder $svReadsDIR does not exist ... creating"     
-    
-fi
+    # Software and location
+    R_version="/software/R-3.5.0/bin/"
+    go_version="/software/team151/gcc-8.1.0/bin/"
 
-# For sample (in x)  (Probably run one sample per job in an array - maybe list the samples, and then bsub a job array. 
-# for now (maybe this will have to be in a sub script?)
+    ## A number of folders need to be created for the intermediate file steps. 
+    ## This statement will delete EVERYTHING, and will replace it with new ones. 
 
-    sample="EGAN00001214492"
-    software=$software 
-    # pass in relevant DIRs
-
-        STinDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/"
-        STinTab="GS_filtered_DEL_CNV.txt"
-        STsample=${sample}
-        SToutDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/"
-        STsoftware=${software}
-
-        /software/R-3.4.0/bin/Rscript ${scriptDIR}/separateTable.R   -f ${STinDIR} -d ${STinDIR} -i ${STsample} -o ${SToutDIR} -s ${STsoftware}
-
-        RBCindex="/lustre/scratch115/projects/interval_wgs/testBams/EGAN00001207556.bam.bai"
-        RBCbam="/lustre/scratch115/projects/interval_wgs/testBams/EGAN00001207556.bam"
-        RBCint="smolInt.txt"
-        RBCout="/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/reads_${software}"
-        RBCsampleName=${sample}
-
-        echo "running readBamChunks.go to get folder of read files per SV"
-        echo "go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
-         -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}"
+    # The root folder. 
+        if [ -d ${wkDIR} ]; then
+            echo "your directory ${wkDIR} already existed.... ...I have replaced it. All." 
+            rm -rf ${wkDIR}
+            mkdir ${wkDIR}
+        else
+            mkdir ${wkDIR}
+            echo "Folder ${wkDIR} didn't exist ... creating it for you!"
+        fi
         
-        go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
-         -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}
+    #### Make the output tables: 
+   
+        # separateTable.R - makes one SV coord table per sample
+            SToutDIR="${wkDIR}/${software}-raw_tables"
+        # readBamChunks.go - makes one file of reads per SV, per sample
+            RBCoutDIR="${wkDIR}/${software}-reads"
+        # assignBins.R - gives reads height for a plot
+            ABoutDIR="${wkDIR}/${software}-binned"
+        # splitCigar - splits constructs into cigar constituents
+            SCoutDIR="${wkDIR}/${software}-cigar_split"
+        # viewRegions - makes the plots. 
+            VRoutDIR="${wkDIR}/${software}-plots"
 
-        ABinDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/reads/"
-        ABinTab="EGAN00001214492-homDel-CNV_chr1_86644_90107-reads.txt"
-        ABoutDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/binnedTables"
-
-        echo "running assignBins.R to assign the reads a height"
-        echo "Rscript assignBins.R -d ${ABinDIR} -f ${ABinTab} -o ${ABoutDIR}"
-
-        Rscript ${scriptDIR}/assignBins.R -d ${ABinDIR} -f ${ABinTab} -o ${ABoutDIR}
-
-        SCbinFile="/Users/bh10/Documents/Rotations/Rotation3/data/testView/binnedTables/EGAN00001214492-homDel-CNV_chr1_86644_90107-binned.txt"
-        SCoutDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/splitTables/"
-        SCoutFile="EGAN00001214492-homDel-CNV_chr1_86644_90107-split.txt"
-
-        echo "running splitCigar.go to expand the cigar strings"
-        echo "go run ${scriptDIR}/splitCigar/splitCigar.go -binFile=${SCbinFile} -outPath=${SCoutDIR} -outFile=${SCoutFile}"
-
-        go run ${scriptDIR}/splitCigar/splitCigar.go -binFile=${SCbinFile} -outPath=${SCoutDIR} -outFile=${SCoutFile}
+            mkdir ${SToutDIR}
+            mkdir ${RBCoutDIR}
+            mkdir ${ABoutDIR}
+            mkdir ${SCoutDIR}
+            mkdir ${VRoutDIR}
 
 
-        VRinDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/splitTables/"
-        VRinTab="EGAN00001214492-homDel-CNV_chr1_86644_90107-split.txt"
-        VRoutDIR="/Users/bh10/Documents/Rotations/Rotation3/data/testView/cov_read_plots"
+        
+        ## Use raw GS table, separate out the relevant colums for the current sample. Can only have non-reference, or all. 
 
-        echo "running viewRegions.R to produce plots"
-        echo "Rscript viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}"
-        Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}
 
+        STinDIR=${rawTabDIR}  ## Where the raw table is
+        STinTab="GS_filtered_DEL_CNV.txt"                                   ## Name of the raw table
+        STsample=${sample}
+        STsoftware=${software}
+        # made in the R script, rather than for an argument
+        stOutInterValTable="${STinTab%.txt}_${sample}.txt" 
+
+        # File with original data
+        if [ -f ${STinDIR}/${STinTab} ]; then
+            echo "file $STinDIR exists ...continuing" 
+        else
+            echo "Folder $STinDIR does not exist ... exiting"     
+            exit
+        fi
+
+            echo "running separateTable.R to pull out ${whichSVs} SVs from the original table"
+            echo "call: ${Rversion}/Rscript ${scriptDIR}/separateTable.R   -f ${STinDIR} -d ${STinDIR} -i ${STsample} -o ${SToutDIR} -s ${STsoftware} -w ${whichSVs}"
+            
+            ${Rversion}/Rscript ${scriptDIR}/separateTable.R   -f ${STinDIR} -d ${STinDIR} -i ${STsample} -o ${SToutDIR} -s ${STsoftware} -w ${whichSVs}
+
+        ## Retrieve list of intervals for the sample from SToutDIR and withdraw the reads
+        ## Put output in RBCout/sample
+        ## This is the script which makes several files, one per SV for the current sample
+
+            RBCindex="${bamDIR}/${sample}.bam.bai"
+            RBCbam="${bamDIR}/${sample}.bam"
+            RBCint="${SToutDIR}/${stOutInterValTable}"
+            RBCsampleName=${sample} 
+
+            echo "running readBamChunks.go to get reads per SV"
+            echo "Call: ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
+            -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}"
+            
+            ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
+            -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}
+
+
+        ## Retrieve list of read tables from RBCout and assign them a bin value
+        ## Put output in ABoutDIR/sample
+
+            ABinDIR=${RBCout}
+            listReads=$(ls ${ABinDIR}/*.txt)
+
+            for readTable in ${listReads} ; do 
+                ABinTab=${readTable}
+
+                echo "running assignBins.R to assign the reads a height for ${ABinTab}"
+                echo "Call: Rscript assignBins.R -d ${ABinDIR} -f ${ABinTab} -o ${ABoutDIR}"
+
+                ${Rversion}/Rscript ${scriptDIR}/assignBins.R -d ${ABinDIR} -f ${ABinTab} -o ${ABoutDIR}
+            done
+
+        ## Retrieve binned tables from ABoutDIR and split the records into their cigar constituents
+        ## Put output in SCoutDIR/sample
+
+            SCbinDIR=${ABoutDIR}   ## source files in ABoutDIR
+            listBinnedReads=$(ls ${SCbinDIR}/*.txt)
+        
+            for binnedReadTable in ${listBinnedReads} ; do
+                SCbinFile=${binnedReadTable}
+
+                echo "running splitCigar.go to expand the cigar strings for ${SCbinFile}"
+                echo "Call: ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}"
+                ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}
+            done
+
+        ## Retrieve split tables from SCoutDIR and plot them. 
+        ## Put output in VRoutDIR/sample
+
+            VRinDIR=${SCoutDIR}                         ## source files in SC outDIR
+            listSplitTables=$(ls ${VRinDIR}/*.txt)     ## Lists all files without absolute path
+
+            for splitTable in ${listSplitTables} ; do
+                VRinTab=${splitTable}
+                echo "running viewRegions.R to produce plots for ${VRinTab}"
+                echo "Call: ${Rversion}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}"
+                ${Rversion}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}
+            done
 
