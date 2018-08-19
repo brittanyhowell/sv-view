e45b79f7653d4ce8246091d1a86f77ab795b427c
brittanyhowell <brittany.howell1@gmail.com>
2018-08-19 23:39:48 +0100

    Tidies after itself

--- extract_regions.sh
+++ extract_regions.sh
@@ -32,7 +32,7 @@
 
     # Scripts and junk
     scriptDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/scripts/
-    wkDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/trial-aug13/
+    wkDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/viewSV/trial-aug19/
     plotDIR="${wkDIR}/plots/${sample}"
     mkdir -p ${plotDIR}
     
@@ -47,72 +47,72 @@
     ## A number of folders need to be created for the intermediate file steps. 
     ## This statement will delete EVERYTHING, and will replace it with new ones. 
 
-    # # The root folder. 
-    #     if [ -d "${wkDIR}/${sample}" ]; then
-    #         echo "your directory ${wkDIR}/${sample} already existed.... ...I have replaced it. All." 
-    #         rm -rf "${wkDIR}/${sample}"
-    #         mkdir -p "${wkDIR}/${sample}"
-    #     else
-    #         mkdir -p "${wkDIR}${sample}"
-    #         echo "Folder ${wkDIR}${sample} didn't exist ... creating it for you!"
-    #     fi
+    # The root folder. 
+        if [ -d "${wkDIR}/${sample}" ]; then
+            echo "your directory ${wkDIR}/${sample} already existed.... ...I have replaced it. All." 
+            rm -rf "${wkDIR}/${sample}"
+            mkdir -p "${wkDIR}/${sample}"
+        else
+            mkdir -p "${wkDIR}${sample}"
+            echo "Folder ${wkDIR}${sample} didn't exist ... creating it for you!"
+        fi
         
-    # #### Make the output tables: 
+    #### Make the output tables: 
    
-    #     # separateTable.R - makes one SV coord table per sample
-    #         SToutDIR="${wkDIR}/${sample}/${software}-raw_tables"
+        # separateTable.R - makes one SV coord table per sample
+            SToutDIR="${wkDIR}/${sample}/${software}-raw_tables"
         # readBamChunks.go - makes one file of reads per SV, per sample
             RBCoutDIR="${wkDIR}/${sample}/${software}-reads"
         # assignBins.R - gives reads height for a plot
             ABoutDIR="${wkDIR}/${sample}/${software}-binned"
-    #     # splitCigar - splits constructs into cigar constituents
-    #         SCoutDIR="${wkDIR}/${sample}/${software}-cigar_split"
-    #     # viewRegions - makes the plots. 
-    #         VRoutDIR="${wkDIR}/${sample}/${software}-plots"
+        # splitCigar - splits constructs into cigar constituents
+            SCoutDIR="${wkDIR}/${sample}/${software}-cigar_split"
+        # viewRegions - makes the plots. 
+            VRoutDIR="${wkDIR}/${sample}/${software}-plots"
 
-    #         mkdir -p ${SToutDIR}
-    #         mkdir -p ${RBCoutDIR}
-    #         mkdir -p ${ABoutDIR}
-    #         mkdir -p ${SCoutDIR}
-    #         mkdir -p ${VRoutDIR}
+            mkdir -p ${SToutDIR}
+            mkdir -p ${RBCoutDIR}
+            mkdir -p ${ABoutDIR}
+            mkdir -p ${SCoutDIR}
+            mkdir -p ${VRoutDIR}
 
 
         
-    #     ## Use raw GS table, separate out the relevant colums for the current sample. Can only have non-reference, or all.  
-    #     # made in the R script, rather than for an argument
-    #     stOutInterValTable="${STinTab%.txt}_${sample}.txt" 
+        ## Use raw GS table, separate out the relevant colums for the current sample. Can only have non-reference, or all.  
+        # made in the R script, rather than for an argument
+        stOutInterValTable="${STinTab%.txt}_${sample}.txt" 
 
-    #     # File with original data
-    #     if [ -f ${STinDIR}/${STinTab} ]; then
-    #         echo "file $STinDIR exists ...continuing" 
-    #     else
-    #         echo "Folder $STinDIR does not exist ... exiting"     
-    #         exit
-    #     fi
+        # File with original data
+        if [ -f ${STinDIR}/${STinTab} ]; then
+            echo "file $STinDIR exists ...continuing" 
+        else
+            echo "Folder $STinDIR does not exist ... exiting"     
+            exit
+        fi
 
-    #         echo "running separateTable.R to pull out ${whichSVs} SVs from the original table"
-    #         echo "call: ${R_version}/Rscript ${scriptDIR}/separateTable.R    -f ${STinTab}      -d ${STinDIR}      -i ${sample}    -o ${SToutDIR}     -s ${software}    -w ${whichSVs}  "
+            echo "running separateTable.R to pull out ${whichSVs} SVs from the original table"
+            echo "call: ${R_version}/Rscript ${scriptDIR}/separateTable.R    -f ${STinTab}      -d ${STinDIR}      -i ${sample}    -o ${SToutDIR}     -s ${software}    -w ${whichSVs}  "
             
-    #         ${R_version}/Rscript ${scriptDIR}/separateTable.R    --inTab=${STinTab}      -d ${STinDIR}      -i ${sample}    -o ${SToutDIR}     -s ${software}    -w ${whichSVs}   
+            ${R_version}/Rscript ${scriptDIR}/separateTable.R    --inTab=${STinTab}      -d ${STinDIR}      -i ${sample}    -o ${SToutDIR}     -s ${software}    -w ${whichSVs}   
 
-    #     ## Retrieve list of intervals for the sample from SToutDIR and withdraw the reads
-    #     ## Put output in RBCout/sample
-    #     ## This is the script which makes several files, one per SV for the current sample
+        ## Retrieve list of intervals for the sample from SToutDIR and withdraw the reads
+        ## Put output in RBCout/sample
+        ## This is the script which makes several files, one per SV for the current sample
 
-    #         RBCindex="${bamDIR}/${sample}.bam.bai"
-    #         RBCbam="${bamDIR}/${sample}.bam"
-    #         RBCint="${SToutDIR}/${stOutInterValTable}"
-    #         RBCsampleName=${sample} 
+            RBCindex="${bamDIR}/${sample}.bam.bai"
+            RBCbam="${bamDIR}/${sample}.bam"
+            RBCint="${SToutDIR}/${stOutInterValTable}"
+            RBCsampleName=${sample} 
 
-    #         echo "running readBamChunks.go to get reads per SV"
-    #         echo "Call: ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
-    #         -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}"
+            echo "running readBamChunks.go to get reads per SV"
+            echo "Call: ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam}
+            -intFile=${RBCint} -outPath=${RBCout} -sampleName=${RBCsampleName}"
             
-    #         ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam} -intFile=${RBCint} -outPath=${RBCoutDIR} -sample=${RBCsampleName}
+            ${go_version}/go run ${scriptDIR}/readBamChunks/readBamChunks.go -index=${RBCindex} -bam=${RBCbam} -intFile=${RBCint} -outPath=${RBCoutDIR} -sample=${RBCsampleName}
 
 
-        ## Retrieve list of read tables from RBCout and assign them a bin value
-        ## Put output in ABoutDIR/sample
+        # Retrieve list of read tables from RBCout and assign them a bin value
+        # Put output in ABoutDIR/sample
 
             ABinDIR=${RBCoutDIR}
             cd ${ABinDIR}
@@ -127,30 +127,44 @@
                 ${R_version}/Rscript ${scriptDIR}/assignBins.R -d ${ABinDIR} -f ${ABinTab} -o ${ABoutDIR}
             done
 
-        # ## Retrieve binned tables from ABoutDIR and split the records into their cigar constituents
-        # ## Put output in SCoutDIR/sample
+            echo "removing reads directory: ${RBCoutDIR}"
+            rm -r ${RBCoutDIR}
 
-        #     SCbinDIR=${ABoutDIR}   ## source files in ABoutDIR
-        #     listBinnedReads=$(ls ${SCbinDIR}/*.txt)
+           ## Retrieve binned tables from ABoutDIR and split the records into their cigar constituents
+           ## Put output in SCoutDIR/sample
+
+            SCbinDIR=${ABoutDIR}   ## source files in ABoutDIR
+            cd ${SCbinDIR}
+            listBinnedReads=$(ls *.txt)
         
-        #     for binnedReadTable in ${listBinnedReads} ; do
-        #         SCbinFile=${binnedReadTable}
+            for binnedReadTable in ${listBinnedReads} ; do
+                SCbinFile=${binnedReadTable}
 
-        #         echo "running splitCigar.go to expand the cigar strings for ${SCbinFile}"
-        #         echo "Call: ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}"
-        #         ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}
-        #     done
+                echo "running splitCigar.go to expand the cigar strings for ${SCbinFile}"
+                echo "Call: ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}"
+                ${go_version}/go run ${scriptDIR}/splitCigar/splitCigar.go -inDIR=${SCbinDIR} -binFile=${SCbinFile} -outPath=${SCoutDIR}
+            done
+        
+        echo "removing binned tables DIR: ${ABoutDIR}"
+        rm -r ${ABoutDIR}
 
-        # ## Retrieve split tables from SCoutDIR and plot them. 
-        # ## Put output in VRoutDIR/sample
+        ## Retrieve split tables from SCoutDIR and plot them. 
+        ## Put output in VRoutDIR/sample
 
-        #     VRinDIR=${SCoutDIR}                         ## source files in SC outDIR
-        #     listSplitTables=$(ls ${VRinDIR}/*.txt)     ## Lists all files without absolute path
+            VRinDIR=${SCoutDIR}                         ## source files in SC outDIR
+            cd ${VRinDIR}
+            listSplitTables=$(ls *.txt)     ## Lists all files without absolute path
 
-        #     for splitTable in ${listSplitTables} ; do
-        #         VRinTab=${splitTable}
-        #         echo "running viewRegions.R to produce plots for ${VRinTab}"
-        #         echo "Call: ${R_version}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}"
-        #         ${R_version}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}
-        #     done
+            for splitTable in ${listSplitTables} ; do
+                VRinTab=${splitTable}
+                echo "running viewRegions.R to produce plots for ${VRinTab}"
+                echo "Call: ${R_version}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}"
+                ${R_version}/Rscript ${scriptDIR}/viewRegions.R -d ${VRinDIR} -f ${VRinTab} -o ${VRoutDIR}
+            done
+        
+        echo "removing split tables DIR: ${SCoutDIR}"
+        rm -r ${SCoutDIR}
+
+        echo "complete, exiting"
+
 
